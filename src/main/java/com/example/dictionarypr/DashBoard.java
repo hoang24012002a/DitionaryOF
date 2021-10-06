8632cff41ee788e9100bfd7f6b5a755d4cdde79a
Truongvnu <th0375178055@gmail.com>
2021-10-06 07:52:37 +0700

    run App here

---
+++ src/main/java/com/example/dictionarypr/DashBoard.java
@@ -0,0 +1,24 @@
+package com.example.dictionarypr;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.kordamp.bootstrapfx.BootstrapFX;
+
+public class DashBoard extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("dashboard.fxml"));
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
