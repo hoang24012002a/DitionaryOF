7bc838df2ce40ad0af22a749299812abbabb7de4
Truongvnu <th0375178055@gmail.com>
2021-10-06 07:54:51 +0700

    moi method dieu khie, xu li event app thao tac tai day

---
+++ src/main/java/com/example/dictionarypr/DashBoardController.java
@@ -0,0 +1,15 @@
+package com.example.dictionarypr;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DashBoardController {
+
+}
+
