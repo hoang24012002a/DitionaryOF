54e72188d9e3445ba853151ebde27ce62b2c354e
Truongvnu <th0375178055@gmail.com>
2021-10-09 08:15:58 +0700

    update setting.java

--- src/main/java/com/example/dictionarypr/Setting.java
+++ src/main/java/com/example/dictionarypr/Setting.java
@@ -1,4 +1,10 @@
 package com.example.dictionarypr;
 
+/**
+ * @Tuan.
+ * Tham khao tai day https://www.youtube.com/watch?v=Ln0BFlx6R4w
+ * Thay doi text WOMEN <-> MEN (Do ko can thiep dc bang ScenceBuiler
+ * */
+
 public class Setting {
 }
