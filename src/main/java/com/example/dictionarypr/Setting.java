939179378de7fa40081f46c63a7b79bd298f9bce
Truongvnu <th0375178055@gmail.com>
2021-10-24 01:10:08 +0700

    make effect

--- src/main/java/com/example/dictionarypr/Setting.java
+++ src/main/java/com/example/dictionarypr/Setting.java
@@ -1,22 +1,41 @@
 package com.example.dictionarypr;
 
+import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextArea;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import animatefx.animation.*;
+import animatefx.util.ParallelAnimationFX;
+import animatefx.util.SequentialAnimationFX;
+import javafx.fxml.Initializable;
+import javafx.scene.text.Text;
+import javafx.scene.transform.Rotate;
 
-public class Setting {
+import java.awt.*;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Setting implements Initializable {
     private final TextToText textToTextEngine = new TextToText();
 
     @FXML
     private JFXTextArea engText;
     @FXML
     private JFXTextArea viText;
+    @FXML
+    private JFXButton tranButton;
 
     @FXML
     protected void renderText(ActionEvent keyEvent) {
         String eng = engText.getText();
         String vi = textToTextEngine.translate(eng);
+        new RotateIn(tranButton).play();  /** ???? Why it not work ??? **/
         viText.setText(vi);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
 
     }
 }
