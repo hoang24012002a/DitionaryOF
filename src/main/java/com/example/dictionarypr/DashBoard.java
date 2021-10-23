b46284b82b2d0f45b6547212d4c105d8035dadca
Truongvnu <th0375178055@gmail.com>
2021-10-24 01:07:53 +0700

    nothing

--- src/main/java/com/example/dictionarypr/DashBoard.java
+++ src/main/java/com/example/dictionarypr/DashBoard.java
@@ -6,22 +6,21 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
-import org.kordamp.bootstrapfx.BootstrapFX;
 
 public class DashBoard extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+  public static void main(String[] args) {
+    launch(args);
+  }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("dashboard.fxml"));
-        Scene scene = new Scene(root);
-//        primaryStage.initStyle(StageStyle.UNDECORATED);
-        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("images/app_icon.png")));
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    Parent root = FXMLLoader.load(getClass().getResource("dashboard.fxml"));
+    primaryStage.setTitle("Dictionary App");
+    Scene scene = new Scene(root);
+    //primaryStage.initStyle(StageStyle.UNDECORATED);
+    primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("images/app_icon.png")));
+    primaryStage.setScene(scene);
+    primaryStage.show();
+  }
 }
