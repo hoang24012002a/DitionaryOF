32684e3338e9ee7571b5479f26b2fe79d452ad76
Truongvnu <th0375178055@gmail.com>
2021-10-25 13:57:41 +0700

    nothing change

--- src/main/java/com/example/dictionarypr/DictionaryCommandline.java
+++ src/main/java/com/example/dictionarypr/DictionaryCommandline.java
@@ -1,4 +1,5 @@
 package com.example.dictionarypr;
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -144,7 +145,7 @@ public class DictionaryCommandline {
         try {
             Class.forName("com.mysql.jdbc.Driver");
             Connection con = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/chinh_sua", "root", "");
+                    "jdbc:mysql://localhost:3306/chinhsua", "root", "");
             //chinhsua là tên của database, root là username và password là rỗng
             Statement stmt = con.createStatement();
             ResultSet rs = stmt.executeQuery("SELECT * FROM tbl_edict WHERE word LIKE '" + keyWord + "%'");
